ee3dfc8d716b3cf7690d650b1a09e093cb9e341d
Carlos Badilla <carlosbadillac@outlook.com>
2025-06-30 18:45:33 -0400

    Desarrollo de ejercicio 3: Nombres de funciones, argumentos y parámetros

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,48 @@
+(() => {
+    
+    // función para obtener información de una película por Id
+    function getMovieById( id: string ) {
+        console.log({ id });
+    }
+    
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCast( id: string ) {
+        console.log({ id });
+    }
+    
+    // funcion para obtener el bio del actor por el id
+    function getActorById( id: string ) {
+        console.log({ id });
+    }
+    
+    interface Movie {
+        id: string;
+        title: string;
+        description: string;
+        rating: number;
+        cast: string[];
+    }
+
+    // Crear una película
+    function createMovie( { title, description, rating, cast }: Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    function checkFullName( fullName: string ): boolean {
+        console.log({ fullName });
+        return true;
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( checkFullName('fernando') ) return false;
+
+        console.log('Crear actor');
+        return true;
+    }
+
+})();
